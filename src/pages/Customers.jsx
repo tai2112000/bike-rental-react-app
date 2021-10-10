daae0c59c826765cd3907bd22f7d5ac7c4074a2c
tai2112000 <71784448+tai2112000@users.noreply.github.com>
2021-10-10 19:23:19 +0700

    test API
    test API

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,56 +1,85 @@
-import React from 'react'
+import React, { Component } from "react";
 
-import Table from '../components/table/Table'
+import Table from "../components/table/Table";
 
-import customerList from '../assets/JsonData/customers-list.json'
+import customerList from "../assets/JsonData/customers-list.json";
+import callApi from "../utils/apiCaller";
 
 const customerTableHead = [
-    '',
-    'name',
-    'email',
-    'phone',
-    'total orders',
-    'total spend',
-    'location'
-]
+  //   "",
+  //   "name",
+  //   "email",
+  //   "phone",
+  //   "total orders",
+  //   "total spend",
+  //   "location",
 
-const renderHead = (item, index) => <th key={index}>{item}</th>
+  "Postal Code",
+  "Area name",
+];
+
+const renderHead = (item, index) => <th key={index}>{item}</th>;
 
 const renderBody = (item, index) => (
-    <tr key={index}>
-        <td>{item.id}</td>
-        <td>{item.name}</td>
-        <td>{item.email}</td>
-        <td>{item.phone}</td>
-        <td>{item.total_orders}</td>
-        <td>{item.total_spend}</td>
-        <td>{item.location}</td>
-    </tr>
-)
+  <tr key={index}>
+    <td>{item.postalCode}</td>
+    <td>{item.name}</td>
+    {/* <td>{item.email}</td>
+    <td>{item.phone}</td>
+    <td>{item.total_orders}</td>
+    <td>{item.total_spend}</td>
+    <td>{item.location}</td> */}
+  </tr>
+);
 
-const Customers = () => {
+class Customers extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listArea: [],
+      apiURL: "",
+    };
+  }
+
+  componentDidMount() {
+    callApi("areas?page=1", "GET", null).then((res) => {
+      this.setState({
+        listArea: res.data.data,
+      });
+    });
+  }
+  render() {
+    const { listArea } = this.state;
+
+    console.log(listArea);
     return (
-        <div>
-            <h2 className="page-header">
-                customers
-            </h2>
-            <div className="row">
-                <div className="col-12">
-                    <div className="card">
-                        <div className="card__body">
-                            <Table
-                                limit='10'
-                                headData={customerTableHead}
-                                renderHead={(item, index) => renderHead(item, index)}
-                                bodyData={customerList}
-                                renderBody={(item, index) => renderBody(item, index)}
-                            />
-                        </div>
-                    </div>
-                </div>
+      <div>
+        <h2 className='page-header'>customers</h2>
+        <div className='row'>
+          <div className='col-12'>
+            <div className='card'>
+              <div className='card__body'>
+                <table>
+                  <thead>
+                    <th>Postal Code</th>
+                    <th>Name</th>
+                  </thead>
+                  <tbody>
+                    {listArea.map((item) => (
+                      <tr key={item.id}>
+                        <td>{item.postalCode}</td>
+                        <td>{item.name}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    );
+  }
 }
 
-export default Customers
+export default Customers;
