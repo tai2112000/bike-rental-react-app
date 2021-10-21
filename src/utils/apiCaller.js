755fe00ce93947fed97496e430a296c65753fc6c
tai2112000 <71784448+tai2112000@users.noreply.github.com>
2021-10-21 22:36:25 +0700

    Update call api, add token
    Update call api, add token

--- src/utils/apiCaller.js
+++ src/utils/apiCaller.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 import * as Config from "./../constants/config";
 
-export default function callApi(endpoint, method = "GET", body) {
+export default function callApi(endpoint, method = "GET", body, token = "") {
   return axios({
     method: method,
     url: `${Config.API_URL}/${endpoint}`,
     data: body,
+    headers: {
+      Authorization: "Bearer " + token,
+    },
   }).catch((err) => {
     console.log(err);
   });
