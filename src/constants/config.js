c6c00d4293acfd2fea315a03e9fb3be8e4a4281f
tai2112000 <71784448+tai2112000@users.noreply.github.com>
2021-10-29 07:44:27 +0700

    update  env + config
    update  env + config

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://52.74.12.123/api/v1.0";
-export const API_URL_V2 = "http://52.74.12.123/api/v2";
+export const API_URL = "http://18.138.110.46/api/v1";
+export const API_URL_V2 = "http://18.138.110.46/api/v2";
